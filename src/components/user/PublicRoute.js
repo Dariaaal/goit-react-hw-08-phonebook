51eaa88310306792f00fa12e80f1e1a0543701c9
Dariaaal <1406dariaal@gmail.com>
2023-05-04 23:11:13 +0300

    route

--- src/components/user/PublicRoute.js
+++ src/components/user/PublicRoute.js
@@ -5,9 +5,7 @@ import authSelectors from 'redux/auth/auth-selectors';
 export default function PublicRoute({
     component: Component,
     redirectTo,
-    restricted = false,
 }) {
     const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
-    const shouldRedirect = isLoggedIn && restricted;
-    return shouldRedirect ? <Navigate to={redirectTo}/> : Component
+    return isLoggedIn ? <Navigate to={redirectTo}/> : Component
 }
